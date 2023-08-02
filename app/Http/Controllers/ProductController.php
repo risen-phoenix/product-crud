bb4375975fc1caf8a30d60224bacebca44151a49
Marijke Grobler <marijke.grobler@gmail.com>
2023-08-02 18:45:46 +0200

    Updated to RESTful Api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,50 +4,156 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    public function deleteProduct(Product $product, Request $request){
-        $product->delete();
-
-        return redirect('/');
-    }
-
-    public function updateProduct(Product $product, Request $request)
+    /**
+     * Return a list of products
+     *
+     * @Route("/products')
+     * @Method("GET")
+     *
+     */
+    public function index()
     {
-        $formFieldData = $request->validate([
-            'productName' => ['required'],
-            'productPrice' => ['required'],
-            'productDescription' => ['required']
-        ]);
-
-        $formFieldData['productName'] = strip_tags($formFieldData['productName']);
-        $formFieldData['productPrice'] = strip_tags($formFieldData['productPrice']);
-        $formFieldData['productDescription'] = strip_tags($formFieldData['productDescription']);
-
-        $product->update($formFieldData);
-
-        return redirect('/');
-    }
-    public function showEditProduct(Product $product)
-    {
-        return view('edit-product', ['product' => $product]);
+        $allProducts = Product::all();
+        if ($allProducts->count() > 0) {
+            return response()->json(['status' => 200, 'products' => $allProducts], 200);
+        } else {
+            return response()->json(['status' => 404, 'message' => "No records found"], 404);
+        }
     }
 
+    /**
+     * Add a new product to db
+     * @Route("/products")
+     * @Method("POST")
+     *
+     * @return Response
+     *
+     */
     public function addProduct(Request $request)
     {
-        $formFieldData = $request->validate([
-            'productName' => ['require', 'min:3', 'max:50'],
-            'productPrice' => ['required'],
-            'productDescription' => ['required', 'min:10', 'max:150']
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3|max:50',
+            'price' => 'required | decimal:2',
+            'description' => 'required|string|min:10|max:250'
         ]);
 
-        $formFieldData['productName'] = strip_tags($formFieldData['productName']);
-        $formFieldData['productPrice'] = strip_tags($formFieldData['productPrice']);
-        $formFieldData['productDescription'] = strip_tags($formFieldData['productDescription']);
-        Product::create($formFieldData);
+        if ($validator->fails()) {
+            return response()->json(['status' => 422, 'message' => $validator->messages()], 422);
+        } else {
+            $product = Product::create([
+                'name' => $request->name,
+                'price' => $request->price,
+                'description' => $request->description
+            ]);
 
+            if ($product) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Product successfully created'
+                ], 200);
+            } else {
+                return response()->json(['status' => 500, 'message' => 'Something went wrong'], 500);
+            }
+        }
+    }
 
-        return redirect('/');
+    /**
+     * Returns the product information
+     * @Route("/products/{id}')
+     * @Method("GET")
+     *
+     * @return  Response
+     */
+    public function showProduct($ProductId)
+    {
+        $product = Product::find($ProductId);
+        if ($product) {
+            return response()->json([
+                'status' => 200,
+                'message' => $product
+            ], 200);
+        } else {
+            return response()->json(['status' => 404, 'message' => "Product not found"], 404);
+        }
+    }
+
+    /**
+     * Find product by id
+     *
+     * @Route("/products/{id}/edit")
+     * @Method("GET")
+     *
+     * @return Response
+     *
+     */
+    public function editProduct(int $productId, Request $request)
+    {
+        $product = Product::find($productId);
+
+        if ($product) {
+            return response()->json(['status' => 200, 'product' => $product], 200);
+        } else {
+            return response()->json(['status' => 404, 'message' => "Product not found"], 404);
+        }
+    }
+
+    /**
+     * Update a product
+     *
+     * @Route("/products/{id}/edit")
+     * @Method("PUT")
+     *
+     * @return Response
+     */
+    public function updateProduct(Request $request, int $productId)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3|max:50',
+            'price' => 'required | decimal:2',
+            'description' => 'required|string|min:10|max:250'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['status' => 422, 'message' => $validator->messages()], 422);
+        } else {
+            $product = Product::find($productId);
+
+            if ($product) {
+                $product->update([
+                    'name' => $request->name,
+                    'price' => $request->price,
+                    'description' => $request->description
+                ]);
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Product successfully updated'
+                ], 200);
+            } else {
+                return response()->json(['status' => 404, 'message' => 'No product found'], 500);
+            }
+        }
+    }
+
+    /**
+     * Delete a product
+     *
+     * @Route("/products/{id}/delete")
+     * @Method("DELETE")
+     *
+     * @return Response
+     */
+    public function deleteProduct(int $productId)
+    {
+        $product = Product::find($productId);
+        if ($product) {
+            $product->delete();
+            return response()->json(['status' => 200, 'message' => 'Successfully deleted'], 200);
+        } else {
+            return response()->json(['status' => 404, 'message' => "Product not found"], 404);
+        }
     }
 }
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Name of factory corresponding model instance
+     *
+     */
+    protected $model = Product::class;
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'price' => fake()->randomFloat(2),
+            'description' => fake()->description(),
+        ];
+    }
+}
